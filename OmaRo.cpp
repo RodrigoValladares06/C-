36295752844779ea03261d791dacf3c3dbf89db3
Rodrigo Valladares <158477190+RodrigoValladares06@users.noreply.github.com>
2025-03-04 16:10:56 +0100

    Add files via upload

---
+++ OmaRo.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Rodrigo: Hola Omar, ¿cómo estás?\n");
+    printf("Omar: Hola Rodrigo, estoy bien, ¿y tú?\n");
+    printf("Rodrigo: También estoy bien, gracias. ¿Qué has estado haciendo?\n");
+    printf("Omar: He estado trabajando en un proyecto de programación.\n");
+    printf("Rodrigo: ¡Eso suena interesante! ¿De qué trata?\n");
+    printf("Omar: Es una aplicación para gestionar tareas diarias.\n");
+    printf("Rodrigo: ¡Genial! Me gustaría verla cuando esté lista.\n");
+    printf("Omar: Por supuesto, te la mostraré pronto.\n");
+    printf("Rodrigo: Perfecto, nos vemos luego.\n");
+    printf("Omar: ¡Nos vemos!\n");
+
+    return 0;
+}
