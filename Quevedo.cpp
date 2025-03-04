9856eeac390040364343945814ac7cde21d74164
Rodrigo Valladares <158477190+RodrigoValladares06@users.noreply.github.com>
2025-03-04 18:21:09 +0100

    Add files via upload

---
+++ Quevedo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "      _____  \n";
+    std::cout << "     |     | \n";
+    std::cout << "    ( 0   0 )  \n";
+    std::cout << "     |  ∆  |  < Quevedo \n";
+    std::cout << "      \\___/  \n";
+    std::cout << "     /     \\ \n";
+    std::cout << "    /       \\ \n";
+
+    return 0;
+}
