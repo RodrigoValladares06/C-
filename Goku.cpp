2add53cb6bb4df268aa747fe3a714ca657e788f6
Rodrigo Valladares <158477190+RodrigoValladares06@users.noreply.github.com>
2025-03-04 16:32:04 +0100

    Add files via upload

---
+++ Goku.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "         ,--.\n";
+    std::cout << "       /  .-'\\\n";
+    std::cout << "      |  |  _  )\n";
+    std::cout << "      \\  `-' \\ |\n";
+    std::cout << "       `----'  |\n";
+    std::cout << "        |  |  |\n";
+    std::cout << "     .-'  '--`-.\n";
+    std::cout << "    /          \\\n";
+    std::cout << "   |   GOKU    |\n";
+    std::cout << "    \\        /\n";
+    std::cout << "     `-.  .-'\n";
+    std::cout << "       ||\n";
+    std::cout << "       ||\n";
+
+    return 0;
+}
