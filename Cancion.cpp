bbfd63a9cb7034589acfa1910c39ec4b4ef016c1
Rodrigo Valladares <158477190+RodrigoValladares06@users.noreply.github.com>
2025-03-05 07:00:30 +0100

    Add files via upload

---
+++ Cancion.cpp
@@ -0,0 +1,23 @@
+
+int main() {
+    std::cout << "\n**********************";
+    std::cout << "\n   QUEVEDO - PUNTO G";
+    std::cout << "\n**********************\n";
+    
+    std::cout << "\nBaby, dime si vienes sola\n";
+    std::cout << "Esta noche quiero hacerte cosas\n";
+    std::cout << "Que en verdad te vuelvas loca\n";
+    std::cout << "\n";
+    std::cout << "Tu punto G lo encontré\n";
+    std::cout << "Tú siempre estás en mi mente\n";
+    std::cout << "No sé por qué\n";
+    std::cout << "\n";
+    
+    std::cout << "\n    (\"'"<<"'\") \n";
+    std::cout << "    ( o o )\n";
+    std::cout << "     |   |\n";
+    std::cout << "     |   |\n";
+    std::cout << "     '---'\n";
+    
+    return 0;
+}
